85fc306452f070c5c7641efba9968caa19d5f1f6
Stephangladio <stephangladio2002@gmail.com>
2023-10-11 21:18:11 +0530

    Added javaScript file

---
+++ Script.js
@@ -0,0 +1,8 @@
+
+        var btn = document.getElementById('btn');
+        var light=document.getElementsById("light");
+        
+        function toggleBtn() {
+            btn.classList.toggle("active");
+            ligh.classList.toggle("on");
+        }
